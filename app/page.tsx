cd3e730c8bf120cc41a803f8d0588b8f29c104bf
Stephanie Nguyen <tiennguyen.nltt@gmail.com>
2024-10-26 17:19:36 -0400

    Updated Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
 const Home = () => (
-  <h1 className="text-3xl font-black text-white ">Hello World</h1>
+  <h1 className="text-3xl font-black text-light-500 ">Hello World</h1>
 );
 export default Home;
